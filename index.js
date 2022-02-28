c823e58721a6fa67413295a241ae09e67ca82781
JoDamaso <darryldamaso@gmail.com>
2022-02-28 12:44:53 -0600

    classes created for team

---
+++ index.js
@@ -0,0 +1,16 @@
+// pulling employee's into this js
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const inquirer = require("inquirer");
+const fs = require("fs")
+
+
+const teamMembers = [];
+const idArr = [];
+
+const teamCreate => {
+    inquirer
+        .prompt
+}
