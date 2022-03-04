0d5fbdefb1664b3c4d17d9be1cf613280e2bab14
JoDamaso <darryldamaso@gmail.com>
2022-03-04 13:23:16 -0600

    manager function to create is added

--- index.js
+++ index.js
@@ -38,17 +38,50 @@ function teamCreate () {
 function addManager() {
     inquirer
         .prompt([
-            {
+            {  // manager name 
                 type: "input",
                 name: "managerName",
                 message: "What is your Manager's name you'd like to add?",
                 validate: nameInput => {
                     if (!nameInput == ""){
                         return true;
-                    } return "Please enter atleast a letter"
+                    } else {
+                        console.log("Please enter atleast a letter");
+                        return false;
+                    } 
+                }
+            },
+            { // manager ID
+                type: "input",
+                name: "managerId",
+                message: "What is your Manager's ID Number?",
+                // validate with reg expression
+            },
+            { // manager office number
+                type: "input",
+                name: "managerRoomNumber",
+                message: "What is your Manager's office number?",
+                // validate with reg expression
+            },
+            { // manager email
+                type: "input",
+                name: "managerEmail",
+                message: "What is your Manager's email?",
+                validate: emailInput => {
+                    if (!emailInput == "") {
+                        return true;
+                    } else {
+                        console.log("Please input an email for your Manager");
+                        return false;
+                    }
                 }
             }
-        ])
+        ]).then (inputs => {
+            const manager = new Manager(inputs.managerName, inputs.managerId, inputs.managerEmail, inputs.managerRoomNumber);
+            teamMembers.push(manager);
+            idArr.push(inputs.managerId);
+            teamCreate();
+        })
 } 
 
 // make functions/prompts for Manager, Engineer, and Intern
