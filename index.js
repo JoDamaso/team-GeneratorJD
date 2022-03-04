65786dce88e2264a0d39ca92d755d85c116c43d0
JoDamaso <darryldamaso@gmail.com>
2022-03-04 12:56:11 -0600

    function for creating team made

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-
+// modules 
+const path = require('path');
 const inquirer = require("inquirer");
 const fs = require("fs")
 
@@ -10,7 +11,49 @@ const fs = require("fs")
 const teamMembers = [];
 const idArr = [];
 
-const teamCreate => {
+function teamCreate () {
     inquirer
-        .prompt
-}
+        .prompt([
+            {
+                type: 'list',
+                name: 'members',
+                message: 'Which member would you like to add to your team?',
+                choices: ["Manager", "Engineer", "Intern", "I would not like to choose anymore members!"]
+            }
+        ]).then(employeeChoice => {
+            switch (employeeChoice.members) {
+                case "Manager":
+                    addManager();
+                    break;
+                    case "Engineer":
+                        addEngineer();
+                        break;
+                        case "Intern":
+                            addIntern();
+                            break;
+            }
+        });
+};
+
+function addManager() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is your Manager's name you'd like to add?",
+                validate: nameInput => {
+                    if (!nameInput == ""){
+                        return true;
+                    } return "Please enter atleast a letter"
+                }
+            }
+        ])
+} 
+
+// make functions/prompts for Manager, Engineer, and Intern
+// make sure unique id's and email's if possbile with Regx 
+
+// make sure to push to teamMembers and idArr arrays after prompts with .then 
+
+teamCreate;
