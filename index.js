b4f463dfb735d3119152a1696cfaf3439fcde446
JoDamaso <darryldamaso@gmail.com>
2022-03-04 13:40:17 -0600

    engineer and intern functions added

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ const path = require('path');
 const inquirer = require("inquirer");
 const fs = require("fs")
 
-
+// empty array's we push to after creating a team member
 const teamMembers = [];
 const idArr = [];
 
+// start of our functions
 function teamCreate () {
     inquirer
         .prompt([
@@ -82,11 +83,118 @@ function addManager() {
             idArr.push(inputs.managerId);
             teamCreate();
         })
-} 
+};
 
-// make functions/prompts for Manager, Engineer, and Intern
-// make sure unique id's and email's if possbile with Regx 
+function addEngineer() {
+    inquirer
+        .prompt([
+            {  // engineer name 
+                type: "input",
+                name: "engineerName",
+                message: "What is your Engineer's name you'd like to add?",
+                validate: nameInput => {
+                    if (nameInput !== ""){
+                        return true;
+                    } else {
+                        console.log("Please enter atleast a letter");
+                        return false;
+                    } 
+                }
+            },
+            { // Engineer ID
+                type: "input",
+                name: "engineerId",
+                message: "What is your Engineer's ID Number?",
+                // validate with reg expression
+            },
+            { // Engineer github
+                type: "input",
+                name: "engineerGithub",
+                message: "What is your Engineer's github account?",
+                validate: nameInput => {
+                    if (!nameInput == "") {
+                        return true;
+                    } else {
+                        console.log("Please enter the Engineer's github account");
+                        return false;
+                    }
+                }
+            },
+            { // Engineer email
+                type: "input",
+                name: "engineerEmail",
+                message: "What is your Engineer's email?",
+                validate: emailInput => {
+                    if (emailInput !== "") {
+                        return true;
+                    } else {
+                        console.log("Please input an email for your Engineer");
+                        return false;
+                    }
+                }
+            }
+        ]).then (inputs => {
+            const engineer = new Engineer(inputs.engineerName, inputs.engineerId, inputs.engineerEmail, inputs.engineerGithub);
+            teamMembers.push(engineer);
+            idArr.push(inputs.engineerId);
+            teamCreate();
+        })
+};
 
-// make sure to push to teamMembers and idArr arrays after prompts with .then 
+function addIntern() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "What is your Intern's name you'd like to add?",
+                validate: nameInput => {
+                    if (nameInput !== ""){
+                        return true;
+                    } else {
+                        console.log("Please enter atleast a letter");
+                        return false;
+                    } 
+                }
+            },
+            {
+                type: "input",
+                name: "internId",
+                message: "What is your Intern's ID?",
+                //validate with reg expression
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "What school does your Intern currently attend?",
+                validate: nameInput => {
+                    if (nameInput !== "") {
+                        return true;
+                    } else {
+                        console.log("Please enter what school your intern goes to")
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What is your Intern's email?",
+                validate: emailInput => {
+                    if (emailInput !== "") {
+                        return true;
+                    } else {
+                        console.log("Please input an email for your Intern");
+                        return false;
+                    }
+                }
+            }
+        ]).then (inputs => {
+            const intern = new Intern(inputs.internName, inputs.internId, inputs.internEmail, inputs.internSchool);
+            teamMembers.push(intern);
+            idArr.push(inputs.internId);
+            teamCreate();
+        });
+};
 
 teamCreate;
